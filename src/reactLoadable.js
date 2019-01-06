a886d495ade999467d1c7610f35218544a7470d3
michaeldimmitt <michaelgdimmitt@gmail.com>
2019-01-06 18:04:50 -0500

    error component

--- src/reactLoadable.js
+++ src/reactLoadable.js
@@ -1,6 +1,10 @@
 import Loadable from 'react-loadable';
 import Loading from './components/Loading';
 
+export const Error = Loadable({
+    loader: () => import('./components/Error'),
+    loading: Loading,
+});
 export const Login = Loadable({
     loader: () => import('./components/Login'),
     loading: Loading,
