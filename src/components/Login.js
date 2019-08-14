f9f409941a5d5a5224b886abd73220bab9af6281
michaeldimmitt <michaelgdimmitt@gmail.com>
2019-08-14 17:33:22 -0400

    improve login screen

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import { toQs } from '../lib/queryString'
-
+import '../Login.css'
 export default class Login extends React.Component {
   
   render() {
@@ -19,14 +19,34 @@ export default class Login extends React.Component {
     const url = 'https://secure.meetup.com/oauth2/authorize' + toQs(params)
 
     const Comp = () => <div className="ph3">
-      <div style={{ display:'flex', justifyContent: 'center', alignItems: 'center', width:'300px',height:'100px', margin:'200px', backgroundColor:'powderblue'}}>
+      <div >
+        <br/>
         <a href={url} style={{fontSize:"30px"}}>Click to Login</a>
+        <br/>
+        <br/>
       </div>
     </div>
 
-    return <div>
-      <Comp/>
-    </div>
+    return (
+      <div id="Login-container" className="pd-w-3 ma-center" >
+        <br/><br/><br/>
+        <h1 id="Login-header">Meetup Subscribe (for meetup.com)</h1>
+        <div id="Login-content" className="pd-w-3">
+          look at upcoming events for your meetup groups.<br/><br/>
+          filter to look at events for a specific group.<br/><br/>
+          rsvp "yes" to the next 10 events for a group.<br/><br/>
+          rsvp "no" to the next 10 events for a group.<br/><br/>
+          rsvp to 10 events for all groups.<br/><br/>
+        </div>
+        <br/>
+        <div id="Login-footer" className="pd-r-3">
+          <div>Login to get started</div>
+          <span>Enjoy!</span>
+        </div>
+
+        <Comp/>
+      </div>
+    )
   }
 
 }
