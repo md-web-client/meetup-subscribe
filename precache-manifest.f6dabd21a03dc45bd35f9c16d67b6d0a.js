cde05b1355c01870c8ff4ec5dea631a50f5f5e78
michaeldimmitt <michaelgdimmitt@gmail.com>
2019-08-26 10:01:14 -0400

    Updates

---
+++ precache-manifest.f6dabd21a03dc45bd35f9c16d67b6d0a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8b09b91b057025398ca",
+    "url": "/meetup-subscribe/static/js/4.f8b09b91.chunk.js"
+  },
+  {
+    "revision": "29fcf044e0c4881ab0aa",
+    "url": "/meetup-subscribe/static/js/0.29fcf044.chunk.js"
+  },
+  {
+    "revision": "8cbb34be1b8c336c714c",
+    "url": "/meetup-subscribe/static/js/2.8cbb34be.chunk.js"
+  },
+  {
+    "revision": "51cfaedebe60756c61fc",
+    "url": "/meetup-subscribe/static/js/runtime~main.51cfaede.js"
+  },
+  {
+    "revision": "64444e8a4b4bf0001f15",
+    "url": "/meetup-subscribe/static/js/3.64444e8a.chunk.js"
+  },
+  {
+    "revision": "4229fec41f6181641c3e",
+    "url": "/meetup-subscribe/static/js/main.4229fec4.chunk.js"
+  },
+  {
+    "revision": "6bb4935c5f35141a31a5",
+    "url": "/meetup-subscribe/static/js/5.6bb4935c.chunk.js"
+  },
+  {
+    "revision": "fd08d7753c79a87034f2",
+    "url": "/meetup-subscribe/static/js/6.fd08d775.chunk.js"
+  },
+  {
+    "revision": "ef2b8409ee9a77abb9be",
+    "url": "/meetup-subscribe/static/js/7.ef2b8409.chunk.js"
+  },
+  {
+    "revision": "64444e8a4b4bf0001f15",
+    "url": "/meetup-subscribe/static/css/3.228b590e.chunk.css"
+  },
+  {
+    "revision": "5c7dc44af210682339f4ce22d29e2635",
+    "url": "/meetup-subscribe/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/meetup-subscribe/precache-manifest.f6dabd21a03dc45bd35f9c16d67b6d0a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/meetup-subscribe/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
